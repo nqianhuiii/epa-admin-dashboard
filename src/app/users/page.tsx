75319d9faccec48f397b0ad45be39d491b053278
nqianhuiii <nghui@graduate.utm.my>
2025-06-20 04:18:43 +0800

    add use client

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,11 +1,6 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { UserTable } from "@/components/user-table";
 import { getAllUsers } from "@/services/userSercvice";
-// import { TopChannels } from "@/components/Tables/top-channels";
-// import { TopChannelsSkeleton } from "@/components/Tables/top-channels/skeleton";
-// import { TopProducts } from "@/components/Tables/top-products";
-// import { TopProductsSkeleton } from "@/components/Tables/top-products/skeleton";
-
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
